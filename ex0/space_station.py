f7985488f9400dd5167c28c5729222c8f46a06cc
Logann Leriche <lleriche@c1r6p3.42lehavre.fr>
2026-02-25 17:17:37 +0100

    module9

--- ex0/space_station.py
+++ ex0/space_station.py
@@ -2,4 +2,11 @@ from pydantic import BaseModel
 
 
 class SpaceStation(BaseModel):
-    station_id: 
+    station_id: str
+    name: str
+    crew_size: int
+    power_level: float
+    oxygen_level: float
+    # last_maintenance:
+    is_operational: bool
+    notes: str
