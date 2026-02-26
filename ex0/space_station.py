1a9cc5e09a2c867f1a126d2ea0c410be6c77ed47
Logann Leriche <lleriche@c1r6p3.42lehavre.fr>
2026-02-26 10:38:37 +0100

    module9

--- ex0/space_station.py
+++ ex0/space_station.py
@@ -1,12 +1,58 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 
 class SpaceStation(BaseModel):
     station_id: str = Field(min_length=3, max_length=10)
-    name: str = Field(max_length=1, min_length=30)
-    crew_size: int = Field(min_digits=1, max_digits=20)
+    name: str = Field(min_length=1, max_length=30)
+    crew_size: int = Field(ge=1, le=20)
     power_level: float = Field(ge=0, le=100)
     oxygen_level: float = Field(ge=0, le=100)
-    # last_maintenance:
+    last_maintenance: datetime = Field(default_factory=datetime.now)
     is_operational: bool = True
-    notes: str = Field(max_length=200)
+    notes: str = Field(default="", max_length=200)
+
+    def get_info(self) -> str:
+        return (f"Station {self.station_id}\n"
+                f"Nom: {self.name}\n"
+                f"Crew: {self.crew_size} people\n"
+                f"Power: {self.power_level}%\n"
+                f"Oxygen: {self.oxygen_level}%\n"
+                f"Status operationel: {self.is_operational}")
+
+
+def main():
+    print("Space Station Data Validation")
+    print("========================================")
+    print("Valid station created:")
+    data = {
+        "station_id": "ISS-01",
+        "name": "Alpha One",
+        "crew_size": 5,
+        "power_level": 95.5,
+        "oxygen_level": 98.0}
+    try:
+        station = SpaceStation(**data)
+        print("✅ Succès !")
+        print(station.get_info())
+    except Exception as e:
+        print(f"❌ Erreur : {e}")
+    print("========================================")
+    data2 = {
+        "station_id": "ISS-01",
+        "name": "Alpha One",
+        "crew_size": 21,
+        "power_level": 95.5,
+        "oxygen_level": 98.0,
+    }
+
+    try:
+        station = SpaceStation(**data2)
+        print("✅ Succès !")
+        print(station.get_info())
+    except Exception as e:
+        print(f"❌ Erreur : {e}")
+
+
+if __name__ == "__main__":
+    main()
