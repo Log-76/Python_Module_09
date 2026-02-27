964ac5a26ad482be88c79a85d2ab6dad8e20e306
Logann Leriche <lleriche@c1r6p3.42lehavre.fr>
2026-02-27 12:27:00 +0100

    module09

--- ex2/space_crew.py
+++ ex2/space_crew.py
@@ -20,6 +20,9 @@ class CrewMember(BaseModel):
     years_experience: int = Field(ge=0, le=50)
     is_active: bool = True
 
+    def get_info(self) -> str:
+        return f"- {self.name} ({self.rank}) - {self.specialization}"
+
 
 class SpaceMission(BaseModel):
     mission_id: str = Field(min_length=5, max_length=15)
@@ -27,7 +30,7 @@ class SpaceMission(BaseModel):
     destination: str = Field(min_length=3, max_length=50)
     launch_date: datetime = Field(default_factory=datetime.now)
     duration_days: int = Field(ge=1, le=3650)
-    crew: CrewMember = Field(min_length=1, max_length=12)
+    crew: list[CrewMember] = Field(min_length=1, max_length=12)
     mission_status: str = Field(default="planned")
     budget_millions: float = Field(ge=1.0, le=10000.0)
 
@@ -35,36 +38,94 @@ class SpaceMission(BaseModel):
     def check_mission(self) -> None:
         if self.mission_id[0] != "M":
             raise Exception("Mission ID must start with 'M'")
-        if self.crew != "Commander" and self.crew != "Captain":
+        for i in self.crew:
+            if i.rank == Rank.commander or i.rank == Rank.captain:
+                break
+        if i.rank != Rank.commander and i.rank != Rank.captain:
             raise Exception("Must have at least one Commander or Captain")
-        if self.duration_days > 365 and self.crew.years_experience < 5:
+        if self.duration_days > 365 and i.years_experience < 5:
             raise Exception("Long missions (> 365 days) need 50%"
                             " experienced crew (5+ years)")
-        if self.crew.is_active is False:
-            raise Exception("All crew members must be active")
+        for i in self.crew:
+            if i.is_active is False:
+                raise Exception("All crew members must be active")
+        return self
+
+    def get_info(self) -> str:
+        return (f"Mission: {self.mission_name}\n"
+                f"ID:  {self.mission_id}\n"
+                f"Destination: {self.destination} \n"
+                f"Duration: {self.duration_days}\n"
+                f"Budget: {self.budget_millions} M\n"
+                f"Crew size: {len(self.crew)}")
 
 
 def main() -> None:
     print("Space Mission Crew Validation")
     print("========================================")
     print("Valid mission created:")
+    Sarah = {"member_id": "sc1_01",
+             "name": "Sarah Connor",
+             "rank": "commander",
+             "age": 25,
+             "specialization": "Mission Command",
+             "years_experience": 6}
+    John = {"member_id": "js1",
+            "name": "John Smith",
+            "rank": "lieutenant",
+            "age": 40,
+            "specialization": "Navigation",
+            "years_experience": 3}
+    Alice = {"member_id": "aj1",
+             "name": "Alice Johnson",
+             "rank": "officer",
+             "age": 20,
+             "specialization": "Engineering",
+             "years_experience": 2}
     member = []
-    member.append(CrewMember("sc1", "Sarah Connor", "commander", 25,
-                             "Mission Command", 6))
-    member.append(CrewMember("js1", "John Smith", "lieutenant", 40,
-                             "Navigation", 6))
-    member.append(CrewMember("aj1", "Alice Johnson", "officer", 20,
-                             "Engineering", 6))
+    member.append(CrewMember(**Sarah))
+    member.append(CrewMember(**John))
+    member.append(CrewMember(**Alice))
     data = {
         "mission_id": "M2024_MARS",
         "mission_name": "Mars Colony Establishment",
         "destination": "Mars",
         "duration_days": 900,
-        "crew": 60,
+        "crew": member,
         "budget_millions": 2500.0}
     try:
         mission = SpaceMission(**data)
         print("✅ Succès !")
         mission.check_mission()
+        print(mission.get_info())
+        print("Crew members:")
+        for crew in mission.crew:
+            print(crew.get_info())
     except Exception as e:
         print(f"❌ Erreur : {e}")
+
+    member2 = []
+    member2.append(CrewMember(**Alice))
+    member2.append(CrewMember(**John))
+    data = {
+        "mission_id": "M2024_MARS",
+        "mission_name": "Mars Colony Establishment",
+        "destination": "Mars",
+        "duration_days": 900,
+        "crew": member2,
+        "budget_millions": 2500.0}
+    print("========================================")
+    print("Expected validation error:")
+    try:
+        mission = SpaceMission(**data)
+        print("✅ Succès !")
+        mission.check_mission()
+        print(mission.get_info())
+        print("Crew members:")
+        for crew in mission.crew:
+            print(crew.get_info())
+    except Exception as e:
+        print(f"❌ Erreur : {e}")
+
+
+main()
